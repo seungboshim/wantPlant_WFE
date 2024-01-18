eb5ac539e124f9716b33e55647858350b7b63d30
Seungbo Shim <fodo9898@gmail.com>
2024-01-19 00:21:56 +0900

    feat: 글로벌스타일 및 색상코드 작성

--- front-end/src/styles/Theme.jsx
+++ front-end/src/styles/Theme.jsx
@@ -7,6 +7,38 @@ const theme = {
         green05: '#739073',
         green06: '#4F6F53',
         green07: '#3A4E39',
+        green08: '#707769',
+        gray01: '#757373',
+        gray02: '#BDBDBD',
+        pink01: '#FFD3C0',
+        pink02: '#FFF5EE',
+
+        purpleBg: '#EFDFFF',
+        purpleText: '#7932C0',
+        greenBg: '#E4FFDF',
+        greenText: '#22A337',
+        redBg: '#FFEBEB',
+        redText: '#E92C2C',
+        orangeBg: '#FFF4E8',
+        orangeText: '#FF9F2D',
+        blueBg: '#E5F3FF',
+        blueText: '#0085FF',
+        pinkBg: '#FCDFFF',
+        pinkText: '#E03AC5',
+
+        tag01: '#B8CDBF',
+        tag02: '#A9B388',
+        tag03: '#739073',
+        tag04: '#4F6F53',
+        tag05: '#EDE3CE',
+        tag06: '#D4C29E',
+        tag07: '#AD9972',
+        tag08: '#7A6740',
+
+        grayBg: '#E8E8E8',
+        grayText: '#585757',
+
+        alert: '#FF0000',
     }
 }
 
