b15fe3e3526518f65d8131ea3e2cc5156e3c8855
tjdvyzl <tjdvyzl12@naver.com>
2024-01-22 21:12:12 +0900

    feat: 정원 수정 모달 구현

--- front-end/src/pages/garden/GardenPage.jsx
+++ front-end/src/pages/garden/GardenPage.jsx
@@ -1,21 +1,33 @@
+import React, { useState } from "react";
 import styled from "styled-components";
-
 import GardenFirst from "./GardenFirst";
 import GardenSecond from "./GardenSecond";
+import EditGardenModal from "../../components/modal/EditGardenModal";
 
 export default function GardenPage() {
-    return (
-        <Wrapper>
-            {/** TODO
-             * 현재 garden_id가 무엇인지에 따라 서로 다른 정원 데이터 불러오기
-             * id에 해당하는 건 First, 그 외는 Second
-             */}
-            <GardenFirst />
-            <GardenSecond />
-        </Wrapper>
-    )
+  const [isEditGardenModalOpen, setIsEditGardenModalOpen] = useState(false);
+
+  const EditGardenModalHandler = (isOpen) => {
+    setIsEditGardenModalOpen(isOpen);
+    document.body.style.overflow = isOpen ? "hidden" : "unset";
+  };
+
+  return (
+    <Wrapper>
+      {/** TODO
+       * 현재 garden_id가 무엇인지에 따라 서로 다른 정원 데이터 불러오기
+       * id에 해당하는 건 First, 그 외는 Second
+       */}
+      <GardenFirst EditGardenModalHandler={EditGardenModalHandler} />
+      <GardenSecond />
+      <EditGardenModal
+        isOpen={isEditGardenModalOpen}
+        EditGardenModalHandler={EditGardenModalHandler}
+      />
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
-    width: 1280px;
-`
+  width: 1280px;
+`;
