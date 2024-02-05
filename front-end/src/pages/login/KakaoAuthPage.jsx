d739d6a84dae30e23db479653db53e934e94c51c
Seungbo Shim <fodo9898@gmail.com>
2024-02-05 20:29:20 +0900

    feat: 토큰 발급받기

--- front-end/src/pages/login/KakaoAuthPage.jsx
+++ front-end/src/pages/login/KakaoAuthPage.jsx
@@ -1,10 +1,17 @@
+<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getAccessToken, getKakaoAccessToken } from '../../apis/login/login';
+=======
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { getKakaoAccessToken } from '../../apis/login/login';
+>>>>>>> b97aba3 (feat: 토큰 발급받기)
 import splitAuthCode from '../../apis/kakao/SplitAuthCode';
 
 /** 카카오 로그인 성공시 리다이렉트될 때 호출 */
 export default function KakaoAuthPage() {
+<<<<<<< HEAD
     const navigate = useNavigate();
     const authCode = splitAuthCode();
     const [kakaoToken, setKakaoToken] = useState("");
@@ -31,6 +38,13 @@ export default function KakaoAuthPage() {
             navigate('/garden/study')
         }
     }, [localStorage.getItem("access")])
+=======
+    const authCode = splitAuthCode();
+
+    if (authCode) {
+        getKakaoAccessToken(authCode);
+    }
+>>>>>>> b97aba3 (feat: 토큰 발급받기)
 
     return (
         <div>loading...</div>
