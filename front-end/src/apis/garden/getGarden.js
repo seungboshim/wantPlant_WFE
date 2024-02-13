600c0e688319a43c377023de90b15814323e4743
Seungbo Shim <fodo9898@gmail.com>
2024-02-13 17:26:41 +0900

    feat: 정원 관련 api 작성

---
+++ front-end/src/apis/garden/getGarden.js
@@ -0,0 +1,39 @@
+import { Server } from "../setting";
+
+export const getEntireGardens = async() => {
+    try {
+        const response = await Server.get(`/api/gardens`);
+        return response.data.result;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getGardensById = async({ gardenId }) => {
+    try {
+        const response = await Server.get(`/api/gardens/${gardenId}`);
+        return response.data.result;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+/** page size = 5로 됐지 왜 */
+/** 정원+페이지네이션 조회, 카테고리 없이 */
+export const getGardensWithPage = async({ page }) => {
+    try {
+        const response = await Server.get(`/api/gardens?page=${page}`);
+        return response.data.result;
+    } catch (error) {
+        console.log(error);
+    }
+}
+/** 정원+페이지네이션 조회, 카테고리 필터링 */
+export const getGardensForCategoryWithPage = async({ page, category }) => {
+    try {
+        const response = await Server.get(`/api/gardens?page=${page}&gardenCategory=${category}`);
+        return response.data.result;
+    } catch (error) {
+        console.log(error);
+    }
+}
