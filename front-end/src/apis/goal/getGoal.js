1c0ffa3302cdbe254c6832768002a8a29d567d0a
Seungbo Shim <fodo9898@gmail.com>
2024-02-13 18:41:26 +0900

    feat: 목표 api 작쇼ㅓㅇ

---
+++ front-end/src/apis/goal/getGoal.js
@@ -0,0 +1,12 @@
+import { Server } from "../setting";
+
+/** 화분 당 goalList, todoList 반환 */
+export const getGoalsByPotId = async ({ potId }) => {
+    try {
+        const response = await Server.get(`/api/goals/todos?potId=${potId}`) // 쿼리 스트링은 이렇게 쓰면 됩니다
+        return response.data.result.goalList;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
